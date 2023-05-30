bde283f702d5dd4f21d6d6885a40bcbea82b383d
Juan Sierra <sierra.juan.1982@gmail.com>
2023-05-29 23:46:46 -0300

    Se crea archivo de helpers

---
+++ app/helpers.php
@@ -0,0 +1,23 @@
+<?php
+
+function generarCodigo ($valor, $module){
+
+    switch ($module) {
+        case 'p':
+            $letras = 'P-BK';
+            break;
+        case 'c':
+            $letras = 'C-BK';
+            break;
+        default:
+            # code...
+            break;
+    }
+    //$letras = 'BK';
+    $longitudTotal = 9;
+
+    $cantidadCeros = $longitudTotal - strlen($letras) - strlen($valor);
+    $codigo = $letras . '-' . str_repeat('0', $cantidadCeros) . $valor;
+
+    return $codigo;
+}
