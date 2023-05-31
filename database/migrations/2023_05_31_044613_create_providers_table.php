fe7e8d8b905d682377f023e2fd073425736251bd
Juan Sierra <sierra.juan.1982@gmail.com>
2023-05-31 02:20:48 -0300

    Se crea migracion para proveedor y tabla pivote

---
+++ database/migrations/2023_05_31_044613_create_providers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('providers', function (Blueprint $table) {
+            $table->id();
+            
+            $table->char('codigo_proveedor')->nullable();
+            $table->char('direccion')->nullable();
+            $table->integer('telefono')->nullable();
+            $table->char('mail');
+            $table->char('razon_social');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('providers');
+    }
+};
