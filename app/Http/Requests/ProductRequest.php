550be0c9b76b4dd4bebdc65fef301ce288d85696
Juan Sierra <sierra.juan.1982@gmail.com>
2023-05-31 02:20:12 -0300

    Se crea request de productos

---
+++ app/Http/Requests/ProductRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'descripcion'   => 'required|min:5|regex:/^[a-zA-ZñÑáéíóúÁÉÍÓÚ0-9\.\- ]+$/i',
+            'precio_compra' => 'required|numeric|decimal:1,3',
+            'stock'         => 'required|numeric',
+            'stock_minimo'  => 'required|numeric'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'descripcion.required'      => 'El Campo descripcion es obligatorio.',
+            'descripcion.min'           => 'El campo descripcion debe tener almenos 5 caracteres.',
+            'precio_compra.required'    => 'El Campo precio es obligatorio.',
+            'precio_compra.numeric'     => 'El Campo precio debe ser numérico.',
+            'precio_compra.decimal'     => 'El Campo precio debe tener entre 1 y 3 decimales.',
+            'stock.required'            => 'El campo Stock es obligatorio',
+            'stock.numeric'             => 'El campo Stock debe ser numérico',
+            'stock_minimo.required'     => 'El campo Stock Minimo es obligatorio',
+            'stock_minimo.numeric'      => 'El campo Stock Minimo debe ser numérico',
+
+        ];
+    }
+}
