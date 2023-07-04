76740f897edd57896d7fdb465b1c4a8c54c310ce
Juan Sierra <sierra.juan.1982@gmail.com>
2023-07-04 10:44:00 -0300

    Se crean las nuvas migraciones

---
+++ database/migrations/2023_06_27_001648_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('brand_id');
+            $table->foreign('brand_id')->references('id')->on('brands');
+
+            $table->char('codigo')->nullable();
+            $table->char('descripcion');
+            $table->integer('stock')->default(0);
+            $table->integer('stock_minimo')->default(0);
+            $table->decimal('precio_compra', 8, 3);
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
